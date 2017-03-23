55e14ea35c4945b164ae7b4456e7f8a8fe8a6aad
Mark-A-A <markawai01@gmail.com>
2017-03-23 16:08:11 -0400

    my first python function

--- pythonrefresher.py
+++ pythonrefresher.py
@@ -18,3 +18,11 @@ if year < 2100 and year > 2000:
     print('Welcome to the 21st century!')
 else:
     print('You are before or after the 21st century')
+
+
+
+def sayHello():
+    print("Hello World")
+
+sayHello()
+sayHello()

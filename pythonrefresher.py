2af594ba2038fda79bd7df6f8f9eaa37ace7967b
Mark-A-A <markawai01@gmail.com>
2017-03-23 15:40:38 -0400

    variables and string concatentation

---
+++ pythonrefresher.py
@@ -0,0 +1,12 @@
+age = 27
+
+name = "Mark"
+
+sentence = "I am " + str(age)
+
+nameSentence = "My name  is {}".format(name)
+ageSentence ="I am {} years old.".format(age)
+
+print(sentence)
+print(nameSentence)
+print(ageSentence)

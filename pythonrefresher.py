4043bb7d4c418e9e423b536cb469fa1cee702f5f
Mark-A-A <markawai01@gmail.com>
2017-03-23 15:57:09 -0400

    python uses : at the end of if statements

--- pythonrefresher.py
+++ pythonrefresher.py
@@ -11,3 +11,10 @@ sentence4 = "My name  is {} and I am {} years old.".format(name,age)
 # print(nameSentence)
 # print(ageSentence)
 print(sentence4)
+
+year = 1830 # When you check your solution, don't change this number'
+
+if year < 2100 and year > 2000:
+    print('Welcome to the 21st century!')
+else:
+    print('You are before or after the 21st century')
